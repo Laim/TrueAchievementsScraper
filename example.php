6545f5e34480851424e88ca6407d3669588ca4a3
Laim (iMac) <laim@megafuckinglolz.com>
2017-05-06 22:39:14 +0100

    Uploading example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+$username = "NiiNJA+WEASEL"; //REPLACE ANY SPACES WITH +
+
+require("autoload.php");
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>TrueAchievementsScrapper Example</title>
+  </head>
+  <body>
+
+    <h1>Username, Gamerscore and Avatar Example</h1>
+    <p>Hello <?php echo($TAS->TASGamertag());?>. It looks like you have <?php echo($TAS->TASGamerscore());?> gamerscore.</p>
+
+    <img src="<?php echo($TAS->TASAvatar());?>"/>
+
+  </body>
+</html>
