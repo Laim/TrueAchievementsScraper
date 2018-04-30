e0e07da65ac77746333b706261a3d6320d20f170
Laim <laim@megafuckinglolz.com>
2018-04-30 23:39:46 +0100

    PREG_REPLACE added to replace spaces with plus symbol

--- autoload.php
+++ autoload.php
@@ -1,7 +1,9 @@
 <?php
 require("include/simple_html_dom.php");
 
-$html = file_get_html("https://www.trueachievements.com/gamer/" . $gamertag);
+
+
+$html = file_get_html("https://www.trueachievements.com/gamer/" . preg_replace('/\s+/', '+', $gamertag));
 require("include/functions.php");
 
 $TAS = new TAS();
--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?php
-$gamertag = "NiiNJA+WEASEL"; //REPLACE ANY SPACES WITH +
+$gamertag = "NiiNJA WEASEL"; //REPLACE ANY SPACES WITH +
 
 require("autoload.php");
 ?>
@@ -11,6 +11,7 @@ require("autoload.php");
   <body>
 
     <h1>Username, Gamerscore and Avatar Example</h1>
+
     <p>Hello <?php echo($TAS->TASGamertag());?>. It looks like you have <?php echo($TAS->TASGamerscore());?> gamerscore.</p>
 
     <img src="<?php echo($TAS->TASAvatar());?>"/>
